fd4fa2152d09ff2a7be360a9b56bad89e1f84c79
Osama Abu Safieh <131916263+Osamaas93@users.noreply.github.com>
2023-12-21 14:52:14 +0200

    Update script.js
    script update

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ tgl.onclick = function () {
   }
 };
 
+
 const favouriteBtn = document.getElementById('favouriteBtn');
 const favouritesArea = document.getElementById('favouritesArea');
 
@@ -87,27 +88,36 @@ function getIdFromUrl() {
 
   // Check if the 'id' is found in the URL
   if (match) {
-      // Extract the id from the matched results
-      return match[1];
+    // Extract the id from the matched results
+    return match[1];
   } else {
-/*       console.error("No 'id' found in the URL"); */
-      return null;
+    /*       console.error("No 'id' found in the URL"); */
+    return null;
   }
 }
 
 // Function to fetch and render details
 async function fetchAndRenderDetails() {
-  // Get the id from the URL
-  const id = getIdFromUrl();
+  try {
+    // Get the id from the URL
+    const id = getIdFromUrl();
 
-  if (id !== null) {
+    if (id !== null) {
       // Call fetchDetailes function with the extracted id
-      await fetchDetailes(id);
+      const detailsData = await fetchDetailes(id);
+
+      // Now you have the detailsData, you can render it
+      renderDetailes(detailsData);
+    }
+  } catch (error) {
+    // Handle the error, e.g., display an error message to the user.
+    console.error("Error:", error.message);
   }
 }
-
 // Call the function to fetch and render details when the script is loaded
 fetchAndRenderDetails();
+
+
 
 async function fetchDetailes(uniqueClassNumber) {
   try {
@@ -117,10 +127,12 @@ async function fetchDetailes(uniqueClassNumber) {
     }
     const detailesData = await response.json();
 
-    renderDetailes(detailesData);  
+    return detailesData;
 
   } catch (error) {
-     console.error("Error fetching DETAILES data:", error.message); 
+    console.error("Error fetching DETAILES data:", error.message);
+    // You might want to throw the error here to handle it in the calling code.
+    throw error;
   }
 }
 
@@ -128,8 +140,8 @@ async function fetchDetailes(uniqueClassNumber) {
 
 //Function That Render The Detailes Page 
 function renderDetailes(detailesData) {
-    
-    try {
+
+  try {
     const aboutDiv = document.querySelector('.aboutCourse');
 
     const categoryR = document.createElement('h3');
@@ -200,8 +212,56 @@ function renderDetailes(detailesData) {
     }
 
   } catch (error) {
-     console.error("Error rendering data:", error.message);
-  } 
+    console.error("Error rendering data:", error.message);
+  }
+}
+
+async function renderFavourites(urlID) {
+  const favDetailes = await fetchDetailes(urlID);
+  try {
+
+    const favContent = document.querySelector('.favouriteCourses');
+
+    const favContainer = document.createElement('a');
+    favContainer.classList.add('course');
+    favContainer.href = `details.html?id=${favDetailes.id}`;
+
+    const favImg = document.createElement('img');
+    favImg.src = `./assets/images/${favDetailes.image}`;
+    favImg.alt = favDetailes.name;
+    favContainer.appendChild(favImg);
+    favContent.appendChild(favContainer);
+
+    const favContentDetailes = document.createElement('div');
+    favContentDetailes.classList.add('courseContent');
+
+    const favTitle = document.createElement('h3');
+    favTitle.classList.add('courseTitle');
+    favTitle.textContent = favDetailes.topic;
+    favContentDetailes.appendChild(favTitle);
+    favContainer.appendChild(favContentDetailes);
+
+    const favRating = document.createElement('div');
+    favRating.classList.add('courseRating');
+    favContentDetailes.appendChild(favRating);
+
+
+    for (let i = 0; i < favDetailes.rating; i++) {
+      const star = document.createElement('ion-icon');
+      star.name = 'star';
+      favRating.appendChild(star);
+    }
+
+    for (let i = favDetailes.rating; i < 4; i++) {
+      const starOutline = document.createElement('ion-icon');
+      starOutline.name = 'star-outline';
+      favRating.appendChild(starOutline);
+    }
+
+  } catch {
+    console.error("Error rendering Favourites:", error.message);
+
+  }
 }
 
 // Fetch the data from the API and call the renderData function
@@ -224,71 +284,70 @@ let counter;
 function renderData(data) {
   const currentUrl = window.location.href;
 
-if (currentUrl.endsWith("index.html")) {
-  try {
-   counter= 0;
-    const allCourses = document.querySelector('.allCourses');
-    allCourses.innerHTML = '';
+  if (currentUrl.endsWith("index.html") || currentUrl.endsWith("") ) {
+    try {
+      counter = 0;
+      const allCourses = document.querySelector('.allCourses');
+      allCourses.innerHTML = '';
 
-    for (let card of data) {
-      const course = document.createElement('a');
-      course.href= `details.html?id=${card.id}`; 
-      course.classList.add('course');
-      course.classList.add(`course-${card.id}`);
-      const image = document.createElement('img');
-      image.src = `./assets/images/${card.image}`;
-      image.alt = card.name;
-      course.appendChild(image);
+      for (let card of data) {
+        const course = document.createElement('a');
+        course.href = `details.html?id=${card.id}`;
+        course.classList.add('course');
+        course.classList.add(`course-${card.id}`);
+        const image = document.createElement('img');
+        image.src = `./assets/images/${card.image}`;
+        image.alt = card.name;
+        course.appendChild(image);
 
-      const courseContent = document.createElement('div');
-      courseContent.classList.add('courseContent');
+        const courseContent = document.createElement('div');
+        courseContent.classList.add('courseContent');
 
-      const cardCategory = document.createElement('p');
-      cardCategory.classList.add('courseSubTitle');
-      cardCategory.innerText = card.category;
-      courseContent.appendChild(cardCategory);
+        const cardCategory = document.createElement('p');
+        cardCategory.classList.add('courseSubTitle');
+        cardCategory.innerText = card.category;
+        courseContent.appendChild(cardCategory);
 
-      const courseTitle = document.createElement('h3');
-      courseTitle.classList.add('courseTitle');
-      courseTitle.innerText = card.topic;
-      courseContent.appendChild(courseTitle);
+        const courseTitle = document.createElement('h3');
+        courseTitle.classList.add('courseTitle');
+        courseTitle.innerText = card.topic;
+        courseContent.appendChild(courseTitle);
 
-      const courseRating = document.createElement('div');
-      courseRating.classList.add('courseRating');
+        const courseRating = document.createElement('div');
+        courseRating.classList.add('courseRating');
 
-      for (let i = 0; i < card.rating; i++) {
-        const star = document.createElement('ion-icon');
-        star.name = 'star';
-        courseRating.appendChild(star);
+        for (let i = 0; i < card.rating; i++) {
+          const star = document.createElement('ion-icon');
+          star.name = 'star';
+          courseRating.appendChild(star);
+        }
+
+        for (let i = card.rating; i < 4; i++) {
+          const starOutline = document.createElement('ion-icon');
+          starOutline.name = 'star-outline';
+          courseRating.appendChild(starOutline);
+        }
+
+        courseContent.appendChild(courseRating);
+
+        const courseAuthor = document.createElement('p');
+        courseAuthor.classList.add('courseAuthor');
+        courseAuthor.innerText = 'Author: ' + card.name;
+        courseContent.appendChild(courseAuthor);
+
+        course.appendChild(courseContent);
+        allCourses.appendChild(course);
+
+        counter++;
+        const countResult = document.querySelector('.resultsNumber');
+        countResult.innerHTML = counter;
       }
+      addCardClickEventListeners();
 
-      for (let i = card.rating; i < 4; i++) {
-        const starOutline = document.createElement('ion-icon');
-        starOutline.name = 'star-outline';
-        courseRating.appendChild(starOutline);
-      }
-
-      courseContent.appendChild(courseRating);
-
-      const courseAuthor = document.createElement('p');
-      courseAuthor.classList.add('courseAuthor');
-      courseAuthor.innerText = 'Author: ' + card.name;
-      courseContent.appendChild(courseAuthor);
-
-      course.appendChild(courseContent);
-      allCourses.appendChild(course);
-
-      counter++;
-      const countResult = document.querySelector('.resultsNumber');
-      countResult.innerHTML = counter;
+    } catch (error) {
+      console.error("Error rendering data:", error.message);
     }
-    addCardClickEventListeners();
-
-  } catch (error) {
-      console.error("Error rendering data:", error.message); 
-  }
-} else {
-}
+  } else {}
 }
 
 
@@ -343,10 +402,6 @@ function applySorting(selectedValue) {
   }
 }
 
-
-
-
-
 // SortData Function by topic and by author 
 const sortData = function () {
   try {
@@ -361,63 +416,62 @@ const sortData = function () {
     // Apply sorting when the page loads
     applySorting(sortList.value);
   } catch (error) {
-   /*  console.error("Error sorting data:", error.message);  */
+    /*  console.error("Error sorting data:", error.message);  */
   }
 }
 
 function applyFiltering(selectedFilterValue) {
   const cards = Array.from(document.querySelectorAll('.course'));
-  counter= 0;
+  counter = 0;
   try {
 
     cards.forEach(card => {
       const courseSubTitle = card.querySelector('.courseSubTitle');
-      if(selectedFilterValue === 'Default'){
+      if (selectedFilterValue === 'Default') {
         card.style.display = 'block';
-      }
-      else if(courseSubTitle.textContent === selectedFilterValue){
-        card.style.display = 'block'; 
+      } else if (courseSubTitle.textContent === selectedFilterValue) {
+        card.style.display = 'block';
         counter++;
-      } else{
-        card.style.display = 'none'; 
+      } else {
+        card.style.display = 'none';
       }
       const countResult = document.querySelector('.resultsNumber');
       countResult.innerHTML = counter;
-  });
+    });
 
 
   } catch (error) {
-   console.error("Error Filtering data:", error.message); 
+    console.error("Error Filtering data:", error.message);
   }
 }
 
 const filterData = function () {
   try {
-      const filterList = document.getElementById('filterList');
+    const filterList = document.getElementById('filterList');
 
-      // Event listener for the 'change' event
-      filterList.addEventListener('change', function () {
-          // Get the selected option
-          const selectedOption = filterList.selectedOptions[0];
+    // Event listener for the 'change' event
+    filterList.addEventListener('change', function () {
+      // Get the selected option
+      const selectedOption = filterList.selectedOptions[0];
 
-          // Check if an option is selected
-          if (selectedOption) {
-              // Get the text content of the selected option
-              const selectedFilterText = selectedOption.textContent;
-              
-              // Call the function with the selected text
-              applyFiltering(selectedFilterText);
-          }
-      });
+      // Check if an option is selected
+      if (selectedOption) {
+        // Get the text content of the selected option
+        const selectedFilterText = selectedOption.textContent;
 
-      // Apply filtering when the page loads
-      // Ensure that an option is selected initially
-      if (filterList.selectedOptions.length > 0) {
-          const selectedFilterText = filterList.selectedOptions[0].textContent;
-          applyFiltering(selectedFilterText);
+        // Call the function with the selected text
+        applyFiltering(selectedFilterText);
       }
+    });
+
+    // Apply filtering when the page loads
+    // Ensure that an option is selected initially
+    if (filterList.selectedOptions.length > 0) {
+      const selectedFilterText = filterList.selectedOptions[0].textContent;
+      applyFiltering(selectedFilterText);
+    }
   } catch (error) {
-  /*     console.error("Error filtering data:", error.message); */
+    /*     console.error("Error filtering data:", error.message); */
   }
 };
 // Call the render function to fetch and render initial data
@@ -426,3 +480,55 @@ render();
 // Call the sortData function to initialize sorting functionality
 sortData();
 filterData();
+
+const getCurrentUrl = function () {
+  return window.location.href;
+
+}
+
+function favouriteButton() {
+  const Url = getCurrentUrl();
+
+  if (Url.includes('details.html')) {
+
+    const favouritesButton = document.querySelector(".favouritesButton");
+
+    favouritesButton.addEventListener("click", handleClick);
+
+    function handleClick() {
+
+      const favButton = document.querySelector(".favouritesButton");
+      const favButtonContent= favButton.textContent.toLowerCase();
+      const urlID = getIdFromUrl();
+    if(favButtonContent.includes('add')){
+      renderFavourites(urlID);
+      favButton.innerText="Remove From Favourites ";
+      const filledHeart= document.createElement('ion-icon');
+      filledHeart.name = 'heart';
+      filledHeart.classList.add('heartLogo');
+      favButton.appendChild(filledHeart);
+      
+    } else{
+      removeFromFavourite(urlID);
+      favButton.innerText="Add to Favourites ";
+      const filledHeart= document.createElement('ion-icon');
+      filledHeart.name = 'heart-outline';
+      filledHeart.classList.add('heartLogo');
+      favButton.appendChild(filledHeart);
+    }
+    }
+
+  };
+}
+function removeFromFavourite(urlID){
+    const removeItem = document.querySelectorAll('.course');
+    removeItem.forEach(anchor => {
+      const hrefValue = anchor.getAttribute('href');
+      if (hrefValue === `details.html?id=${urlID}`){
+        anchor.remove();
+
+      }
+    });    
+}
+
+favouriteButton();
